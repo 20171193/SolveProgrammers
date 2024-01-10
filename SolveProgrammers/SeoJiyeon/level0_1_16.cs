e734282a837db058db74b726dbb3c3eb4b862e6f
wldus-0123 <96jy@naver.com>
2024-01-10 15:13:37 +0900

    프로그래머스 입문 1페이지 16번문제

---
+++ SolveProgrammers/SeoJiyeon/level0_1_16.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolveProgrammers.SeoJiyeon
+{   // 피자 나눠 먹기 (1)
+    internal class level0_1_16
+    {
+    }
+}
