9392f36e500bafb5daf77f506f8eebd2b2d5695f
wldus-0123 <96jy@naver.com>
2024-01-10 17:04:52 +0900

    프로그래머스 입문 2페이지 2번문제

---
+++ SolveProgrammers/SeoJiyeon/level0_2_2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolveProgrammers.SeoJiyeon
+{
+    internal class level0_2_2
+    {   // 배열 두배 만들기
+        public int[] solution(int[] numbers)
+        {
+
+            int[] answer = new int[numbers.Length];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                answer[i] = numbers[i] * 2;
+
+            }
+            return answer;
+        }
+    }
+}
