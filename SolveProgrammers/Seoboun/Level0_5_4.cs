9b556a0c568a25c543f76abe209872d26363baf8
Shim Jae Chun <shimnaldo98@gmail.com>
2024-01-10 16:18:21 +0900

    Update Version1.1

---
+++ SolveProgrammers/Seoboun/Level0_5_4.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolveProgrammers.Seoboun
+{
+    class Level0_5_4
+    {
+        public string solution(string[] id_pw, string[,] db)
+        {
+            string answer = "";
+
+            answer = "fail";
+            for (int i = 0; i < db.GetLength(0); i++)
+            {
+                // id가 같은 상황에서
+                if (id_pw[0] == db[i, 0])
+                {
+                    // pw도 동일하다면
+                    if (id_pw[1] == db[i, 1])
+                    {
+                        // 로그인 성공
+                        answer = "login";
+                    }
+                    // pw가 일치하지 않다면
+                    if (answer != "login")
+                    {
+                        // id는 일치하나 pw는 틀린 상황
+                        answer = "wrong pw";
+                    }
+                }
+            }
+
+            return answer;
+        }
+    }
+}
