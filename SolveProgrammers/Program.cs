dfffbbdb63bcd0ce05739cf85d42ca8a21142a5e
boun SEO <kgjsid@gmail.com>
2024-01-10 15:22:14 +0900

    메인 수정

--- SolveProgrammers/Program.cs
+++ SolveProgrammers/Program.cs
@@ -1,11 +1,12 @@
 ﻿namespace SolveProgrammers
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            // 주석추가
+            Console.WriteLine("프로그래머스 5페이지 1번 문제 풀었다");
+            Console.WriteLine("프로그래머스 5페이지 2번 문제 풀었다");
+            Console.WriteLine("프로그래머스 5페이지 3번 문제 풀었다");
         }
     }
 }
