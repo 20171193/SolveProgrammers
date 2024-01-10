64130e0930feceea9b49e006c01ff290479ade03
boun SEO <kgjsid@gmail.com>
2024-01-10 15:50:29 +0900

    docs : 해결 상황 기록

--- SolveProgrammers/Program.cs
+++ SolveProgrammers/Program.cs
@@ -8,6 +8,7 @@
             Console.WriteLine("프로그래머스 5페이지 2번 문제 풀었다");
             Console.WriteLine("프로그래머스 5페이지 3번 문제 풀었다");
             Console.WriteLine("프로그래머스 5페이지 4번 문제 풀었다");
+            Console.WriteLine("프로그래머스 5페이지 5번 문제 풀었다");
         }
     }
 }
