8284fc88c6d69005e8abed5e631c4fd78a87a362
Shim Jae Chun <shimnaldo98@gmail.com>
2024-01-10 16:47:10 +0900

    Level0_4_1 문제풀이

---
+++ SolveProgrammers/Shimjaechun/Level0_4_1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolveProgrammers.Shimjaechun
+{
+    internal class Level0_4_1
+    {
+        public int solution(string my_string, string is_suffix)
+        {
+            int answer = 0;
+
+            for (int i = 0; i < my_string.Length; i++)
+            {
+                if (is_suffix == my_string.Substring(i, my_string.Length - i)) return 1;
+            }
+
+            return answer;
+        }
+    }
+}
