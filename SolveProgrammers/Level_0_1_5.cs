b8816cdcb068f7b3ec7ddc0a509e0c3711338d19
prorange2023 <prorange2023@gmail.com>
2024-01-10 15:52:20 +0900

    Level_0_1_5 풀이

---
+++ SolveProgrammers/Level_0_1_5.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolveProgrammers
+{
+	internal class Level_0_1_5
+	{
+		public int solution(int num1, int num2)
+		{
+			int result = num1 % num2;
+			return result;
+		}
+	}
+}
