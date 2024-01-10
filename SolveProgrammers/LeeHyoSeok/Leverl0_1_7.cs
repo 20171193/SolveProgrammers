7b590c99bea3b4a9251d843b7ad6f89cf9fd6bd9
lee hyoseok <lhs19960330@gmail.com>
2024-01-10 15:11:32 +0900

    Leverl0_0_7 문제풀이

---
+++ SolveProgrammers/LeeHyoSeok/Leverl0_1_7.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{ // 두 수의 합 
+        int solution(int num1, int num2)
+        {
+            int answer = num1 + num2;
+            return answer;
+        }
+    }
+}
