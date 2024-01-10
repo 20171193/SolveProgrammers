aab042b87a830753e41ff0c79fb78b104c51e3d3
prorange2023 <prorange2023@gmail.com>
2024-01-10 16:51:33 +0900

    fix : main 누적 수정

--- SolveProgrammers/Program.cs
+++ SolveProgrammers/Program.cs
@@ -4,8 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("프로그래머스 1페이지 11번 문제 풀었다");
-            
-        }
+            Console.WriteLine("프로그래머스 1페이지 02번 문제 풀었다");
+			Console.WriteLine("프로그래머스 1페이지 05번 문제 풀었다");
+			Console.WriteLine("프로그래머스 1페이지 08번 문제 풀었다");
+			Console.WriteLine("프로그래머스 1페이지 11번 문제 풀었다");
+
+		}
     }
 }
