abaa05abafaada369a86680b807a9f751c72d52e
wldus-0123 <96jy@naver.com>
2024-01-10 15:19:21 +0900

    프로그래머스 입문 1페이지 16번문제

--- SolveProgrammers/SeoJiyeon/level0_1_16.cs
+++ SolveProgrammers/SeoJiyeon/level0_1_16.cs
@@ -8,5 +8,18 @@ namespace SolveProgrammers.SeoJiyeon
 {   // 피자 나눠 먹기 (1)
     internal class level0_1_16
     {
+        public int solution(int n)
+        {
+            int answer = 0;
+            if (n % 7 == 0)
+            {
+                answer = n / 7;
+            }
+            else
+            {
+                answer = n / 7 + 1;
+            }
+            return answer;
+        }
     }
 }
