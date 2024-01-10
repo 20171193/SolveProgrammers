c637bec8aff0ed23503bb7cc1415caf549b62eee
lee hyoseok <lhs19960330@gmail.com>
2024-01-10 15:58:27 +0900

    프로그래머스 입문 0레벨 1페이지 19번문제

---
+++ SolveProgrammers/LeeHyoSeok/Level0_1_19.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Class1
+{
+    public Class1()
+    {  
+        // 배열 자르기
+
+        public class Solution
+    {
+        public int[] solution(int[] numbers, int num1, int num2)
+        {
+            for (int i = 0; i< numbers.Length; i++)
+            {
+                if(num1<= i && num2 >= i)
+                {
+                    int[] aaa = new int[] { numbers[i] };
+                    return aaa;
+                }
+            }
+
+            
+            return aaa;
+        }
+    }
+}
+}
