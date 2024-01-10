15fdf0ff649a6058fe90bb1b369d1427b6eb20bf
prorange2023 <prorange2023@gmail.com>
2024-01-10 16:50:45 +0900

    Level_0_1_11 풀이

---
+++ SolveProgrammers/HongSeongHyun/Level0_1_11.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolveProgrammers.HongSeongHyun
+{
+	internal class Level0_1_11
+	{
+		public double solution(int[] numbers)
+		{
+
+			double length = numbers.Length;
+			double sum = 0;
+			for (int i = 0; i < numbers.Length; i++)
+			{
+
+				sum += numbers[i];
+			}
+			double result = sum / length;
+
+			return result;
+		}
+	}
+}
