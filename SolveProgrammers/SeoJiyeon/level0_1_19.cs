46e75a216b568c845b429b6f2d8a6584529d6667
wldus-0123 <96jy@naver.com>
2024-01-10 17:01:11 +0900

    프로그래머스 입문 1페이지 19번문제

---
+++ SolveProgrammers/SeoJiyeon/level0_1_19.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolveProgrammers.SeoJiyeon
+{
+    internal class level0_1_19
+        // 배열 자르기
+    {
+        public int[] solution(int[] numbers, int num1, int num2)
+        {
+            int[] answer = new int[num2 - num1 + 1];
+            for (int i = 0; i < answer.Length; i++)
+            {
+                answer[i] = numbers[i + num1];
+            }
+            return answer;
+        }
+    }
+}
